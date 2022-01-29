45556e5d1ccee38d5c013878f9449ed54008961b
parthpankajtiwary <parthpankajtiwary@gmail.com>
2022-01-29 22:33:28 +0100

    add generalisation to dataset and model class for categorical regression

--- hyperparams_optimisation.py
+++ hyperparams_optimisation.py
@@ -22,7 +22,7 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 BATCHSIZE = 8192
 CLASSES = 1
-EPOCHS = 10
+EPOCHS = 15
 DIR = os.getcwd()
 
 def pearson_loss(x, y):
@@ -36,55 +36,104 @@ def swish(x):
     return x * torch.sigmoid(x)
 
 class UbiquantData(Dataset):
-    def __init__(self, data: pd.core.frame.DataFrame):
+    def __init__(self, data: pd.core.frame.DataFrame, categorical=False):
         self.target = data[['target']].values
         self.data = data.drop(['row_id', 'time_id', 'investment_id', 'target', 'Unnamed: 0'], axis=1).values
+        self.investment_ids = data.investment_id.values
+        self.categorical = categorical
 
     def __getitem__(self, idx):
         x_cont = self.data[idx]
         target = self.target[idx]
-        return torch.tensor(x_cont).float(), torch.tensor(target).float()
+        x_cat = self.investment_ids[idx]
+        if self.categorical:
+            return torch.tensor(x_cont).float(), x_cat, torch.tensor(target).float()
+        else:
+            return torch.tensor(x_cont).float(), torch.tensor(target).float()
 
     def __len__(self):
         return len(self.data)
 
 class Net(nn.Module):
-    def __init__(self, dropout: float, output_dims: List[int]):
+    def __init__(self,
+                 dropout_mlp: float,
+                 dropout_emb: float,
+                 output_dims: List[int],
+                 cat_dims: List[int],
+                 emb_output: int,
+                 categorical=False):
+
         super().__init__()
-        layers: List[nn.Module] = []
-
+        mlp_layers: List[nn.Module] = []
+        emb_layers: List[nn.Module] = []
+        self.categorical = categorical
         input_dim: int = 300
-        for output_dim in output_dims:
-            layers.append(nn.Linear(input_dim, output_dim))
-            layers.append(nn.ReLU())
-            layers.append(nn.Dropout(dropout))
-            input_dim = output_dim
+        cat_input_dim: int = 3774
 
-        layers.append(nn.Linear(input_dim, 1))
+        emb_layers.append(nn.Embedding(cat_input_dim, emb_output))
 
-        self.layers: nn.Module = nn.Sequential(*layers)
+        if categorical:
+                cat_input_dim = emb_output
+                for cat_output_dim in cat_dims:
+                    emb_layers.append(nn.Linear(cat_input_dim, cat_output_dim))
+                    emb_layers.append(nn.ReLU())
+                    emb_layers.append(nn.Dropout(dropout_emb))
+                    cat_input_dim = cat_output_dim
 
-    def forward(self, x_cont):
-        return self.layers(x_cont)
+                input_dim += cat_output_dim
 
+                for output_dim in output_dims:
+                    mlp_layers.append(nn.Linear(input_dim, output_dim))
+                    mlp_layers.append(nn.ReLU())
+                    mlp_layers.append(nn.Dropout(dropout_mlp))
+                    input_dim = output_dim
+        else:
+            for output_dim in output_dims:
+                mlp_layers.append(nn.Linear(input_dim, output_dim))
+                mlp_layers.append(nn.ReLU())
+                mlp_layers.append(nn.Dropout(dropout_mlp))
+                input_dim = output_dim
+
+        mlp_layers.append(nn.Linear(input_dim, 1))
+
+        # self.layers: nn.Module = nn.Sequential(*layers)
+        self.emb_nn: nn.Module = nn.Sequential(*emb_layers)
+        self.mlp_nn: nn.Module = nn.Sequential(*mlp_layers)
+
+    def forward(self, x_cont, x_cat):
+        x_cat = self.emb_nn(x_cat)
+        concat = torch.cat([x_cat, x_cont], 1)
+        output = self.mlp_nn(concat)
+        return output
 
 class UbiquantModel(pl.LightningModule):
-    def __init__(self, dropout: float, output_dims: List[int], l_rate: float):
+    def __init__(self,
+                 dropout_mlp: float,
+                 dropout_emb: float,
+                 output_dims: List[int],
+                 emb_dims: List[int],
+                 emb_output: int,
+                 l_rate: float,
+                 categorical: bool):
         super().__init__()
-        self.model = Net(dropout, output_dims)
-        self.model = Net(dropout, output_dims)
+        self.model = Net(dropout_mlp,
+                         dropout_emb,
+                         output_dims,
+                         emb_dims,
+                         emb_output,
+                         categorical=categorical)
         self.l_rate = l_rate
 
-    def forward(self, data: torch.Tensor) -> torch.Tensor:
-        return self.model(data)
+    def forward(self, x_cont, x_cat):
+        return self.model(x_cont, x_cat)
 
     def train_dataloader(self):
-        train_dataset = UbiquantData(train_data)
+        train_dataset = UbiquantData(train_data, categorical=True)
         train_loader = DataLoader(dataset=train_dataset, batch_size=BATCHSIZE)
         return train_loader
 
     def val_dataloader(self):
-        val_dataset = UbiquantData(val_data)
+        val_dataset = UbiquantData(val_data, categorical=True)
         val_loader = DataLoader(dataset=val_dataset, batch_size=BATCHSIZE)
         return val_loader
 
@@ -92,16 +141,16 @@ class UbiquantModel(pl.LightningModule):
         return optim.Adam(self.parameters(), lr=self.l_rate)
 
     def training_step(self, batch, batch_idx):
-        x_cont, y = batch
-        logits = self.forward(x_cont)
+        x_cont, x_cat, y = batch
+        logits = self.forward(x_cont, x_cat)
         loss = pearson_loss(logits, y)
         logs = {'loss': loss}
         return {'loss': loss, 'log': logs}
 
     def validation_step(self, batch, batch_idx):
-        x_cont, y = batch
+        x_cont, x_cat, y = batch
         scores_df = pd.DataFrame(index=range(len(y)), columns=['targets', 'preds'])
-        logits = self.forward(x_cont)
+        logits = self.forward(x_cont, x_cat)
         scores_df.targets = y.cpu().numpy()
         scores_df.preds = logits.cpu().numpy()
         pearson = scores_df['targets'].corr(scores_df['preds'])
@@ -118,17 +167,30 @@ class UbiquantModel(pl.LightningModule):
 
 
 def objective(trial: optuna.trial.Trial) -> float:
-
-    # We optimize the number of layers, hidden units in each layer and dropouts.
-    n_layers = trial.suggest_int("n_layers", 1, 5)
-    dropout = trial.suggest_float("dropout", 0.1, 0.5)
+    n_mlp_layers = trial.suggest_int("n_mlp_layers", 1, 5)
+    n_emb_layers = trial.suggest_int("n_emb_layers", 1, 5)
+    dropout_mlp = trial.suggest_float("dropout_mlp", 0.1, 0.5)
+    dropout_emb = trial.suggest_float("dropout_emb", 0.1, 0.5)
     output_dims = [
-        trial.suggest_int("n_units_l{}".format(i), 32, 512, log=True) for i in range(n_layers)
+        trial.suggest_int("n_units_mlp{}".format(i), 32, 512, log=True) for i in range(n_mlp_layers)
     ]
+    emb_dims = [
+        trial.suggest_int("n_units_emb{}".format(i), 32, 512, log=True) for i in range(n_emb_layers)
+    ]
+
+    emb_output = trial.suggest_int("emb_output", 5, 100)
 
     l_rate = trial.suggest_loguniform('learning_rate', 1e-4, 1e-1)
 
-    model = UbiquantModel(dropout, output_dims, l_rate=l_rate)
+    model = UbiquantModel(dropout_mlp=dropout_mlp,
+                          dropout_emb=dropout_emb,
+                          output_dims=output_dims,
+                          emb_dims=emb_dims,
+                          emb_output=emb_output,
+                          l_rate=l_rate,
+                          categorical=True)
+
+    print(model)
 
     trainer = pl.Trainer(
         logger=True,
@@ -137,7 +199,14 @@ def objective(trial: optuna.trial.Trial) -> float:
         gpus=1,
         callbacks=[PyTorchLightningPruningCallback(trial, monitor="pearson")],
     )
-    hyperparameters = dict(n_layers=n_layers, dropout=dropout, output_dims=output_dims)
+    hyperparameters = dict(n_mlp_layers=n_mlp_layers,
+                           n_emb_layers=n_emb_layers,
+                           dropout_mlp=dropout_mlp,
+                           dropout_emb=dropout_emb,
+                           output_dims=output_dims,
+                           emb_dims=emb_dims,
+                           emb_output=emb_output,
+                           l_rate=l_rate)
     trainer.logger.log_hyperparams(hyperparameters)
     trainer.fit(model)
 
--- train.py
+++ train.py
@@ -5,10 +5,12 @@ import numpy as np
 import torch
 import torch.optim as optim
 import torch.nn as nn
-from torch.utils.data import Dataset, TensorDataset, DataLoader
+from torch.utils.data import Dataset, DataLoader
 import pytorch_lightning as pl
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint
+
+from typing import List
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -18,9 +20,10 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 pl.utilities.seed.seed_everything(seed=2022)
 
-epochs = 5
-l_rate = 1e-3
-mse_loss = nn.MSELoss()
+BATCHSIZE = 8192
+CLASSES = 1
+EPOCHS = 15
+DIR = os.getcwd()
 
 def pearson_loss(x, y):
     xd = x - x.mean()
@@ -38,70 +41,126 @@ def weighted_average(a):
     for j in range(1, n + 1):
         j = 2 if j == 1 else j
         w.append(1 / (2**(n + 1 - j)))
-    return np.average(a, weights = w)
+    return np.average(a, weights=w)
 
 class UbiquantData(Dataset):
-    def __init__(self, data: pd.core.frame.DataFrame):
+    def __init__(self, data: pd.core.frame.DataFrame, categorical=False):
         self.target = data[['target']].values
         self.data = data.drop(['row_id', 'time_id', 'investment_id', 'target'], axis=1).values
         self.investment_ids = data.investment_id.values
+        self.categorical = categorical
 
     def __getitem__(self, idx):
         x_cont = self.data[idx]
         target = self.target[idx]
         x_cat = self.investment_ids[idx]
-        return torch.tensor(x_cont).float(), x_cat, torch.tensor(target).float()
+        if self.categorical:
+            return torch.tensor(x_cont).float(), x_cat, torch.tensor(target).float()
+        else:
+            return torch.tensor(x_cont).float(), torch.tensor(target).float()
 
     def __len__(self):
         return len(self.data)
 
-class UbiquantModel(pl.LightningModule):
-    def __init__(self):
-        super(UbiquantModel, self).__init__()
-        self.emb = nn.Embedding(3774, 64)
-        self.emb_drop = nn.Dropout(0.1)
-        self.bn1 = nn.BatchNorm1d(364)
-        self.fc1 = nn.Linear(64+300, 512)
-        # self.fc1 = nn.Linear(300, 512)
-        self.fc2 = nn.Linear(512, 128)
-        self.fc3 = nn.Linear(128, 64)
-        self.fc4 = nn.Linear(64, 32)
-        self.fc5 = nn.Linear(32, 1)
+class Net(nn.Module):
+    def __init__(self,
+                 dropout_mlp: float,
+                 dropout_emb: float,
+                 output_dims: List[int],
+                 cat_dims: List[int],
+                 emb_output: int,
+                 categorical=False):
+
+        super().__init__()
+        mlp_layers: List[nn.Module] = []
+        emb_layers: List[nn.Module] = []
+        self.categorical = categorical
+        input_dim: int = 300
+        cat_input_dim: int = 3774
+
+        emb_layers.append(nn.Embedding(cat_input_dim, emb_output))
+
+        if categorical:
+                cat_input_dim = emb_output
+                for cat_output_dim in cat_dims:
+                    emb_layers.append(nn.Linear(cat_input_dim, cat_output_dim))
+                    emb_layers.append(nn.ReLU())
+                    emb_layers.append(nn.Dropout(dropout_emb))
+                    cat_input_dim = cat_output_dim
+
+                input_dim += cat_output_dim
+
+                for output_dim in output_dims:
+                    mlp_layers.append(nn.Linear(input_dim, output_dim))
+                    mlp_layers.append(nn.ReLU())
+                    mlp_layers.append(nn.Dropout(dropout_mlp))
+                    input_dim = output_dim
+        else:
+            for output_dim in output_dims:
+                mlp_layers.append(nn.Linear(input_dim, output_dim))
+                mlp_layers.append(nn.ReLU())
+                mlp_layers.append(nn.Dropout(dropout_mlp))
+                input_dim = output_dim
+
+        mlp_layers.append(nn.Linear(input_dim, 1))
+
+        # self.layers: nn.Module = nn.Sequential(*layers)
+        self.emb_nn: nn.Module = nn.Sequential(*emb_layers)
+        self.mlp_nn: nn.Module = nn.Sequential(*mlp_layers)
 
     def forward(self, x_cont, x_cat):
-        x1 = self.emb(x_cat)
-        x1 = self.emb_drop(x1)
+        x_cat = self.emb_nn(x_cat)
+        concat = torch.cat([x_cat, x_cont], 1)
+        output = self.mlp_nn(concat)
+        return output
 
-        x = torch.cat([x1, x_cont], 1)
-        x = swish(self.fc1(x))
-        x = swish(self.fc2(x))
-        x = swish(self.fc3(x))
-        x = swish(self.fc4(x))
-        x = self.fc5(x)
-        return x
+class UbiquantModel(pl.LightningModule):
+    def __init__(self,
+                 dropout_mlp: float,
+                 dropout_emb: float,
+                 output_dims: List[int],
+                 emb_dims: List[int],
+                 emb_output: int,
+                 l_rate: float,
+                 categorical: bool):
+        super().__init__()
+        self.model = Net(dropout_mlp,
+                         dropout_emb,
+                         output_dims,
+                         emb_dims,
+                         emb_output,
+                         categorical=categorical)
+        self.l_rate = l_rate
+        self.categorical = categorical
+
+    def forward(self, x_cont, x_cat):
+        return self.model(x_cont, x_cat)
 
     def train_dataloader(self):
-        train_dataset = UbiquantData(train_data)
-        train_loader = DataLoader(dataset=train_dataset, batch_size=4096)
+        train_dataset = UbiquantData(train_data, categorical=True)
+        train_loader = DataLoader(dataset=train_dataset, batch_size=BATCHSIZE)
         return train_loader
 
     def val_dataloader(self):
-        val_dataset = UbiquantData(val_data)
-        val_loader = DataLoader(dataset=val_dataset, batch_size=4096)
+        val_dataset = UbiquantData(val_data, categorical=True)
+        val_loader = DataLoader(dataset=val_dataset, batch_size=BATCHSIZE)
         return val_loader
 
-    def configure_optimizers(self):
-        return optim.Adam(self.parameters(), lr=l_rate)
-
     def training_step(self, batch, batch_idx):
-        x_cont, x_cat, y = batch
+        if self.categorical:
+            x_cont, x_cat, y = batch
+        else:
+            x_cont, y = batch
         logits = self.forward(x_cont, x_cat)
         loss = pearson_loss(logits, y)
         logs = {'loss': loss}
         return {'loss': loss, 'log': logs}
 
     def validation_step(self, batch, batch_idx):
-        x_cont, x_cat, y = batch
+        if self.categorical:
+            x_cont, x_cat, y = batch
+        else:
+            x_cont, y = batch
         scores_df = pd.DataFrame(index=range(len(y)), columns=['targets', 'preds'])
         logits = self.forward(x_cont, x_cat)
         scores_df.targets = y.cpu().numpy()
@@ -115,12 +174,11 @@ class UbiquantModel(pl.LightningModule):
     def validation_epoch_end(self, outputs):
         avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
         print('mean pearson correlation on validation set: ', avg_loss)
-        if fold not in scores:
-            scores[fold] = [avg_loss]
-        else:
-            scores[fold].append(avg_loss)
         tensorboard_logs = {'val_loss': avg_loss}
         return {'avg_val_loss': avg_loss, 'log': tensorboard_logs}
+
+    def configure_optimizers(self):
+        return optim.Adam(self.parameters(), lr=self.l_rate)
 
 if __name__ == '__main__':
     scores = dict()
@@ -128,30 +186,38 @@ if __name__ == '__main__':
 
     print('data loaded...')
 
-    with open('input/folds_gap15.pickle', 'rb') as handle:
+    with open('input/folds.pickle', 'rb') as handle:
         fold_indexes = pickle.load(handle)
 
-    for fold in fold_indexes:
-        remove_fields = ['target', 'row_id', 'time_id', 'investment_id']
-        target_fields = ['target']
+    fold = 0
 
-        train_data = df.iloc[fold_indexes[fold]['train']].sort_values(by=['time_id'])
-        val_data = df.iloc[fold_indexes[fold]['test']].sort_values(by=['time_id'])
+    remove_fields = ['target', 'row_id', 'time_id', 'investment_id']
+    target_fields = ['target']
 
-        checkpoint_callback = ModelCheckpoint(
-            monitor="pearson",
-            dirpath="models",
-            filename="fold-" + str(fold) + "-ubiquant-mlp-{epoch:02d}-{val_loss:.2f}",
-            save_top_k=1,
-            mode="max",
-        )
+    train_data = df.iloc[fold_indexes[fold]['train']].sort_values(by=['time_id'])
+    val_data = df.iloc[fold_indexes[fold]['test']].sort_values(by=['time_id'])
 
-        model = UbiquantModel()
-        trainer = Trainer(max_epochs=epochs,
-                          fast_dev_run=False,
-                          callbacks=[checkpoint_callback],
-                          gpus=1)
-        trainer.fit(model)
+    checkpoint_callback = ModelCheckpoint(
+        monitor="pearson",
+        dirpath="models",
+        filename="fold-" + str(fold) + "-ubiquant-mlp-{epoch:02d}-{val_loss:.2f}",
+        save_top_k=1,
+        mode="max",
+    )
+
+    model = UbiquantModel(dropout_mlp=0.4217199217221381,
+                          dropout_emb=0.4250209544891712,
+                          output_dims=[508, 405],
+                          emb_dims=[245, 238, 230],
+                          emb_output=56,
+                          l_rate=0.00026840511349794486,
+                          categorical=True)
+
+    trainer = Trainer(max_epochs=EPOCHS,
+                      fast_dev_run=False,
+                      callbacks=[checkpoint_callback],
+                      gpus=1)
+    trainer.fit(model)
 
     score_list = list()
     for fold in scores:
