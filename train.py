3af9156cc263b62b2e68b5cd487be79bffa2ee5e
parthpankajtiwary <parthpankajtiwary@gmail.com>
2022-01-30 23:03:59 +0100

    add inference notebook

--- train.py
+++ train.py
@@ -13,10 +13,12 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 from typing import List
 
 import warnings
+
 warnings.filterwarnings("ignore")
 
 import os
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 pl.utilities.seed.seed_everything(seed=2022)
 
@@ -25,23 +27,27 @@ CLASSES = 1
 EPOCHS = 15
 DIR = os.getcwd()
 
+
 def pearson_loss(x, y):
     xd = x - x.mean()
     yd = y - y.mean()
-    nom = (xd*yd).sum()
-    denom = ((xd**2).sum() * (yd**2).sum()).sqrt()
+    nom = (xd * yd).sum()
+    denom = ((xd ** 2).sum() * (yd ** 2).sum()).sqrt()
     return 1 - nom / denom
+
 
 def swish(x):
     return x * torch.sigmoid(x)
+
 
 def weighted_average(a):
     w = []
     n = len(a)
     for j in range(1, n + 1):
         j = 2 if j == 1 else j
-        w.append(1 / (2**(n + 1 - j)))
+        w.append(1 / (2 ** (n + 1 - j)))
     return np.average(a, weights=w)
+
 
 class UbiquantData(Dataset):
     def __init__(self, data: pd.core.frame.DataFrame, categorical=False):
@@ -57,10 +63,11 @@ class UbiquantData(Dataset):
         if self.categorical:
             return torch.tensor(x_cont).float(), x_cat, torch.tensor(target).float()
         else:
-            return torch.tensor(x_cont).float(), torch.tensor(target).float()
+            return torch.tensor(x_cont).float(), [], torch.tensor(target).float()
 
     def __len__(self):
         return len(self.data)
+
 
 class Net(nn.Module):
     def __init__(self,
@@ -72,29 +79,27 @@ class Net(nn.Module):
                  categorical=False):
 
         super().__init__()
-        mlp_layers: List[nn.Module] = []
-        emb_layers: List[nn.Module] = []
         self.categorical = categorical
+        mlp_layers: List[nn.Module] = []
         input_dim: int = 300
-        cat_input_dim: int = 3774
-
-        emb_layers.append(nn.Embedding(cat_input_dim, emb_output))
 
         if categorical:
-                cat_input_dim = emb_output
-                for cat_output_dim in cat_dims:
-                    emb_layers.append(nn.Linear(cat_input_dim, cat_output_dim))
-                    emb_layers.append(nn.ReLU())
-                    emb_layers.append(nn.Dropout(dropout_emb))
-                    cat_input_dim = cat_output_dim
+            cat_input_dim: int = 3774
+            emb_layers: List[nn.Module] = [nn.Embedding(cat_input_dim, emb_output)]
+            cat_input_dim = emb_output
+            for cat_output_dim in cat_dims:
+                emb_layers.append(nn.Linear(cat_input_dim, cat_output_dim))
+                emb_layers.append(nn.ReLU())
+                emb_layers.append(nn.Dropout(dropout_emb))
+                cat_input_dim = cat_output_dim
 
-                input_dim += cat_output_dim
+            input_dim += cat_output_dim
 
-                for output_dim in output_dims:
-                    mlp_layers.append(nn.Linear(input_dim, output_dim))
-                    mlp_layers.append(nn.ReLU())
-                    mlp_layers.append(nn.Dropout(dropout_mlp))
-                    input_dim = output_dim
+            for output_dim in output_dims:
+                mlp_layers.append(nn.Linear(input_dim, output_dim))
+                mlp_layers.append(nn.ReLU())
+                mlp_layers.append(nn.Dropout(dropout_mlp))
+                input_dim = output_dim
         else:
             for output_dim in output_dims:
                 mlp_layers.append(nn.Linear(input_dim, output_dim))
@@ -104,15 +109,19 @@ class Net(nn.Module):
 
         mlp_layers.append(nn.Linear(input_dim, 1))
 
-        # self.layers: nn.Module = nn.Sequential(*layers)
-        self.emb_nn: nn.Module = nn.Sequential(*emb_layers)
+        if self.categorical:
+            self.emb_nn: nn.Module = nn.Sequential(*emb_layers)
         self.mlp_nn: nn.Module = nn.Sequential(*mlp_layers)
 
     def forward(self, x_cont, x_cat):
-        x_cat = self.emb_nn(x_cat)
-        concat = torch.cat([x_cat, x_cont], 1)
-        output = self.mlp_nn(concat)
+        if self.categorical:
+            x_cat = self.emb_nn(x_cat)
+            concat = torch.cat([x_cat, x_cont], 1)
+            output = self.mlp_nn(concat)
+        else:
+            output = self.mlp_nn(x_cont)
         return output
+
 
 class UbiquantModel(pl.LightningModule):
     def __init__(self,
@@ -149,9 +158,10 @@ class UbiquantModel(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         if self.categorical:
             x_cont, x_cat, y = batch
+            logits = self.forward(x_cont, x_cat)
         else:
-            x_cont, y = batch
-        logits = self.forward(x_cont, x_cat)
+            x_cont, [], y = batch
+            logits = self.forward(x_cont, [])
         loss = pearson_loss(logits, y)
         logs = {'loss': loss}
         return {'loss': loss, 'log': logs}
@@ -159,10 +169,11 @@ class UbiquantModel(pl.LightningModule):
     def validation_step(self, batch, batch_idx):
         if self.categorical:
             x_cont, x_cat, y = batch
+            logits = self.forward(x_cont, x_cat)
         else:
-            x_cont, y = batch
+            x_cont, [], y = batch
+            logits = self.forward(x_cont, [])
         scores_df = pd.DataFrame(index=range(len(y)), columns=['targets', 'preds'])
-        logits = self.forward(x_cont, x_cat)
         scores_df.targets = y.cpu().numpy()
         scores_df.preds = logits.cpu().numpy()
         pearson = scores_df['targets'].corr(scores_df['preds'])
@@ -179,6 +190,7 @@ class UbiquantModel(pl.LightningModule):
 
     def configure_optimizers(self):
         return optim.Adam(self.parameters(), lr=self.l_rate)
+
 
 if __name__ == '__main__':
     scores = dict()
@@ -211,7 +223,9 @@ if __name__ == '__main__':
                           emb_dims=[245, 238, 230],
                           emb_output=56,
                           l_rate=0.00026840511349794486,
-                          categorical=True)
+                          categorical=False)
+
+    print(model)
 
     trainer = Trainer(max_epochs=EPOCHS,
                       fast_dev_run=False,
