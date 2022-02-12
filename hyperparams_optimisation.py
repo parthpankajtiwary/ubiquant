f651b581b7cdaafa1639cde51745a86cd45daff1
parthpankajtiwary <parthpankajtiwary@gmail.com>
2022-02-12 20:38:35 +0100

    add neptune logging to optuna hpo runs

--- hyperparams_optimisation.py
+++ hyperparams_optimisation.py
@@ -1,24 +1,26 @@
-import numpy as np
-
-import argparse
-import os
-from typing import List
-
-import pandas as pd
-
 import optuna
-from optuna.integration import PyTorchLightningPruningCallback
+import argparse
+import pandas as pd
+import numpy as np
+from typing import List
 import pytorch_lightning as pl
 import torch
 from torch import nn
 from torch import optim
 from torch.utils.data import DataLoader, Dataset
+from data.GroupTimeSeriesSplit import GroupTimeSeriesSplit
+from optuna.integration import PyTorchLightningPruningCallback
+import neptune.new.integrations.optuna as optuna_utils
+
+import neptune.new as neptune
 
 import warnings
+
 warnings.filterwarnings("ignore")
 
 import os
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 BATCHSIZE = 8192
 CLASSES = 1
@@ -28,17 +30,15 @@ DIR = os.getcwd()
 def pearson_loss(x, y):
     xd = x - x.mean()
     yd = y - y.mean()
-    nom = (xd*yd).sum()
-    denom = ((xd**2).sum() * (yd**2).sum()).sqrt()
+    nom = (xd * yd).sum()
+    denom = ((xd ** 2).sum() * (yd ** 2).sum()).sqrt()
     return 1 - nom / denom
-
-def swish(x):
-    return x * torch.sigmoid(x)
 
 class UbiquantData(Dataset):
     def __init__(self, data: pd.core.frame.DataFrame, categorical=False):
         self.target = data[['target']].values
-        self.data = data.drop(['row_id', 'time_id', 'investment_id', 'target', 'Unnamed: 0'], axis=1).values
+        self.data = data.drop(['index', 'row_id', 'time_id', 'investment_id', 'target'], axis=1).values
+        # self.data = data.drop(['row_id', 'time_id', 'investment_id', 'target'], axis=1).values
         self.investment_ids = data.investment_id.values
         self.categorical = categorical
 
@@ -49,7 +49,7 @@ class UbiquantData(Dataset):
         if self.categorical:
             return torch.tensor(x_cont).float(), x_cat, torch.tensor(target).float()
         else:
-            return torch.tensor(x_cont).float(), torch.tensor(target).float()
+            return torch.tensor(x_cont).float(), [], torch.tensor(target).float()
 
     def __len__(self):
         return len(self.data)
@@ -64,29 +64,27 @@ class Net(nn.Module):
                  categorical=False):
 
         super().__init__()
-        mlp_layers: List[nn.Module] = []
-        emb_layers: List[nn.Module] = []
         self.categorical = categorical
+        mlp_layers: List[nn.Module] = []
         input_dim: int = 300
-        cat_input_dim: int = 3774
-
-        emb_layers.append(nn.Embedding(cat_input_dim, emb_output))
 
         if categorical:
-                cat_input_dim = emb_output
-                for cat_output_dim in cat_dims:
-                    emb_layers.append(nn.Linear(cat_input_dim, cat_output_dim))
-                    emb_layers.append(nn.ReLU())
-                    emb_layers.append(nn.Dropout(dropout_emb))
-                    cat_input_dim = cat_output_dim
+            cat_input_dim: int = 3774
+            emb_layers: List[nn.Module] = [nn.Embedding(cat_input_dim, emb_output)]
+            cat_input_dim = emb_output
+            for cat_output_dim in cat_dims:
+                emb_layers.append(nn.Linear(cat_input_dim, cat_output_dim))
+                emb_layers.append(nn.ReLU())
+                emb_layers.append(nn.Dropout(dropout_emb))
+                cat_input_dim = cat_output_dim
 
-                input_dim += cat_output_dim
+            input_dim += cat_output_dim
 
-                for output_dim in output_dims:
-                    mlp_layers.append(nn.Linear(input_dim, output_dim))
-                    mlp_layers.append(nn.ReLU())
-                    mlp_layers.append(nn.Dropout(dropout_mlp))
-                    input_dim = output_dim
+            for output_dim in output_dims:
+                mlp_layers.append(nn.Linear(input_dim, output_dim))
+                mlp_layers.append(nn.ReLU())
+                mlp_layers.append(nn.Dropout(dropout_mlp))
+                input_dim = output_dim
         else:
             for output_dim in output_dims:
                 mlp_layers.append(nn.Linear(input_dim, output_dim))
@@ -96,14 +94,17 @@ class Net(nn.Module):
 
         mlp_layers.append(nn.Linear(input_dim, 1))
 
-        # self.layers: nn.Module = nn.Sequential(*layers)
-        self.emb_nn: nn.Module = nn.Sequential(*emb_layers)
+        if self.categorical:
+            self.emb_nn: nn.Module = nn.Sequential(*emb_layers)
         self.mlp_nn: nn.Module = nn.Sequential(*mlp_layers)
 
     def forward(self, x_cont, x_cat):
-        x_cat = self.emb_nn(x_cat)
-        concat = torch.cat([x_cat, x_cont], 1)
-        output = self.mlp_nn(concat)
+        if self.categorical:
+            x_cat = self.emb_nn(x_cat)
+            concat = torch.cat([x_cat, x_cont], 1)
+            output = self.mlp_nn(concat)
+        else:
+            output = self.mlp_nn(x_cont)
         return output
 
 class UbiquantModel(pl.LightningModule):
@@ -123,6 +124,7 @@ class UbiquantModel(pl.LightningModule):
                          emb_output,
                          categorical=categorical)
         self.l_rate = l_rate
+        self.categorical = categorical
 
     def forward(self, x_cont, x_cat):
         return self.model(x_cont, x_cat)
@@ -137,20 +139,25 @@ class UbiquantModel(pl.LightningModule):
         val_loader = DataLoader(dataset=val_dataset, batch_size=BATCHSIZE)
         return val_loader
 
-    def configure_optimizers(self):
-        return optim.Adam(self.parameters(), lr=self.l_rate)
-
     def training_step(self, batch, batch_idx):
-        x_cont, x_cat, y = batch
-        logits = self.forward(x_cont, x_cat)
+        if self.categorical:
+            x_cont, x_cat, y = batch
+            logits = self.forward(x_cont, x_cat)
+        else:
+            x_cont, _, y = batch
+            logits = self.forward(x_cont, [])
         loss = pearson_loss(logits, y)
         logs = {'loss': loss}
         return {'loss': loss, 'log': logs}
 
     def validation_step(self, batch, batch_idx):
-        x_cont, x_cat, y = batch
+        if self.categorical:
+            x_cont, x_cat, y = batch
+            logits = self.forward(x_cont, x_cat)
+        else:
+            x_cont, _, y = batch
+            logits = self.forward(x_cont, [])
         scores_df = pd.DataFrame(index=range(len(y)), columns=['targets', 'preds'])
-        logits = self.forward(x_cont, x_cat)
         scores_df.targets = y.cpu().numpy()
         scores_df.preds = logits.cpu().numpy()
         pearson = scores_df['targets'].corr(scores_df['preds'])
@@ -165,6 +172,8 @@ class UbiquantModel(pl.LightningModule):
         tensorboard_logs = {'val_loss': avg_loss}
         return {'avg_val_loss': avg_loss, 'log': tensorboard_logs}
 
+    def configure_optimizers(self):
+        return optim.Adam(self.parameters(), lr=self.l_rate)
 
 def objective(trial: optuna.trial.Trial) -> float:
     n_mlp_layers = trial.suggest_int("n_mlp_layers", 1, 5)
@@ -188,9 +197,7 @@ def objective(trial: optuna.trial.Trial) -> float:
                           emb_dims=emb_dims,
                           emb_output=emb_output,
                           l_rate=l_rate,
-                          categorical=True)
-
-    print(model)
+                          categorical=False)
 
     trainer = pl.Trainer(
         logger=True,
@@ -220,7 +227,7 @@ if __name__ == "__main__":
         "-p",
         action="store_true",
         help="Activate the pruning feature. `MedianPruner` stops unpromising "
-        "trials at the early stages of training.",
+             "trials at the early stages of training.",
     )
     args = parser.parse_args()
 
@@ -228,21 +235,35 @@ if __name__ == "__main__":
         optuna.pruners.MedianPruner() if args.pruning else optuna.pruners.NopPruner()
     )
 
-    scores = dict()
+    run = neptune.init(
+        project="kaggle.collaboration/ubiquant"
+    )
 
-    train_data = pd.read_csv('input/fold_0_train.csv')
-    val_data = pd.read_csv('input/fold_0_val.csv')
+    df = pd.read_csv("input/train.csv")
+    df = df[(df.time_id <= 355) | (df.time_id >= 420)].reset_index()
 
-    study = optuna.create_study(direction="maximize", pruner=pruner, sampler=optuna.samplers.TPESampler())
-    study.optimize(objective, n_trials=100, timeout=600)
+    gtss = GroupTimeSeriesSplit(n_folds=5, holdout_size=150, groups=df['time_id'])
+    neptune_callback = optuna_utils.NeptuneCallback(run)
 
-    print("Number of finished trials: {}".format(len(study.trials)))
+    for fold, (train_indexes, val_indexes) in enumerate(gtss.split(df)):
+        if fold == 4:
+            train_data = df.iloc[train_indexes].sort_values(by=['time_id'])
+            val_data = df.iloc[val_indexes].sort_values(by=['time_id'])
 
-    print("Best trial:")
-    trial = study.best_trial
+            study = optuna.create_study(direction="maximize", pruner=pruner, sampler=optuna.samplers.TPESampler())
+            study.optimize(objective, n_trials=20, callbacks=[neptune_callback])
 
-    print("  Value: {}".format(trial.value))
+            print("Number of finished trials: {}".format(len(study.trials)))
 
-    print("  Params: ")
-    for key, value in trial.params.items():
-        print("    {}: {}".format(key, value))
+            print("Best trial:")
+            trial = study.best_trial
+
+            print("  Value: {}".format(trial.value))
+
+            print("  Params: ")
+            for key, value in trial.params.items():
+                print("    {}: {}".format(key, value))
+
+            break
+        else:
+            pass
